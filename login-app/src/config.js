b1fa31f19395e1e57ebf34fd08f6878d1dd9408f
ruvennsiow <ruvenn12@gmail.com>
2025-01-19 05:57:37 +0800

    update config

--- login-app/src/config.js
+++ login-app/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:5000'; // Replace with your backend URL when needed
-//const BASE_URL = 'https://quotationtoolfyp.onrender.com'
+//const BASE_URL = 'http://localhost:5000'; // Replace with your backend URL when needed
+const BASE_URL = 'https://quotationtoolfyp.onrender.com'
 module.exports = BASE_URL;
